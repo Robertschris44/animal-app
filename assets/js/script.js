91c04d3da1aa41bfc110c2e0078c0a8b3f287586
Amanda Perry <challenge641@gmail.com>
2022-07-12 19:42:07 -0500

    added modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-var apiUrl = "https://api.humorapi.com/jokes/random?api-key=6a579bfcf6ca422d85fccfe8a0d550c2"
+// get modal element
+var modal = document.getElementById("simpleModal");
+// get open modal button
+var modalBtn = document.getElementById("modalBtn");
+// get close button
+var closeBtn = document.getElementById("closeBtn");
 
-var listEl = document.getElementById("myData");
+// Listen for click
+modalBtn.addEventListener('')
+
+
+var apiUrl = "http://api.nytimes.com/svc/topstories/v2/us.json?api-key=UhYa3vUq6CEUGa2w1YE358ZWzhD1DjEz"
+
+// var listEl = document.getElementById("myData");
 
 fetch(apiUrl)
     .then(function(response){
@@ -16,9 +27,8 @@ fetch(apiUrl)
 
 
     // this works:
-    //  http://api.nytimes.com/svc/semantic/v2/concept/search.json?query=animals&nytd_topic=rescue,shelter,pet&api-key=UhYa3vUq6CEUGa2w1YE358ZWzhD1DjEz
-
-
-    // var apiUrl = "https://dog.ceo/api/breeds/list/all";
+    // "https://api.humorapi.com/jokes/random?api-key=6a579bfcf6ca422d85fccfe8a0d550c2"
+    // "http://api.nytimes.com/svc/semantic/v2/concept/search.json?query=animals&nytd_topic=rescue,shelter,pet&api-key=UhYa3vUq6CEUGa2w1YE358ZWzhD1DjEz
+    // "https://dog.ceo/api/breeds/image/random";
     // "https://api.chucknorris.io/jokes/random"
     // "http://api.nytimes.com/svc/semantic/v2/concept/search.json?query=pets&api-key=UhYa3vUq6CEUGa2w1YE358ZWzhD1DjEz"
