00b4887824448d1238175f8764f4ea5430b8ea5d
Amanda Perry <challenge641@gmail.com>
2022-07-14 14:20:28 -0500

    fixing push

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,12 +19,12 @@ modalBtn.addEventListener('click', function(){
             console.log(result)
             randomDogPic.src = result.message
             localStorage.setItem('randomDogPic', JSON.stringify(result))
-        
+
         })
         .catch(err=>console.log(err));
 })
 
-
+// 
 
 // listen for close click
 closeBtn.addEventListener('click', closeModal);
