2e64be265ec7e08e4a691d01dbd134dae64e228f
Amanda Perry <challenge641@gmail.com>
2022-07-14 18:51:22 -0500

    chris' code

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,19 +64,25 @@ function outsideClick(e){
     }
 }
 
-
-var apiUrl = "https://api.nytimes.com/svc/topstories/v2/us.json?api-key=UhYa3vUq6CEUGa2w1YE358ZWzhD1DjEz"
-
+let apiUrl = "http://api.nytimes.com/svc/topstories/v2/world.json?api-key=UhYa3vUq6CEUGa2w1YE358ZWzhD1DjEz"
+let headlines = document.getElementById('headlines');
 fetch(apiUrl)
-    .then(function(response){
-        return response.json();
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    data.results.map(article => {
+        console.log(article.title);
+        let a = document.createElement("a");
+        a.setAttribute('href', article.url);
+        a.innerHTML = article.title;
+        a.setAttribute("class", "article-title");
+        let p = document.createElement("p");
+        p.innerHTML = article.abstract;
+        let img = document.createElement("img");
+        img.setAttribute('src', article.multimedia[0].url);
+        headlines.appendChild(img);
+        headlines.appendChild(a);
+        headlines.appendChild(p);
     })
-    .then(function(data){
-        console.log(data);
-        
-    })
-    .catch(function(error){
-        console.log(error);
-    });
-
+})
     
